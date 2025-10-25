eb84809e80feb1147bb423374718ec98466a4815
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-10-25 18:54:46 -0300

    Add interpolation function import from scipy

--- tugvalidity.py
+++ tugvalidity.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from scipy.signal import butter, filtfilt, detrend, find_peaks
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 # =========================
