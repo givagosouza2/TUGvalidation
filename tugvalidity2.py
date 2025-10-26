02b77e3dbe2fdc6aa7060a0bea70f03b7383e763
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-10-26 13:04:46 -0300

    Update tugvalidity2.py

--- tugvalidity2.py
+++ tugvalidity2.py
@@ -93,8 +93,11 @@ if uploaded_file_acc:
       ml_acc = np.sqrt(acc_x_filtered**2)
       ap_acc = np.sqrt(acc_z_filtered**2)
 
+    indices, propriedades = find_peaks(v_acc, height = 2, distance = 500)
+    picos_ordenados = np.argsort(ap_acc[indices])[-2:] 
     fig, ax = plt.subplots(figsize=(10, 2))
     ax.plot(time_interpolated[0:2000], v_acc[0:2000], 'k-')
+    ax.plot(time_interpolated[indices], v_acc[indices], 'ro')
     ax.plot([0, 0], [0, 30], 'r--')
     ax.set_xlabel("Tempo (s)")
     ax.set_ylabel("Amplitude")
