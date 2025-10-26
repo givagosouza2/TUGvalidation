b1df94043f129cdfeb856fe966dec93ec9cb0b9b
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-10-26 12:15:35 -0300

    Update tugvalidity2.py

--- tugvalidity2.py
+++ tugvalidity2.py
@@ -101,14 +101,14 @@ if uploaded_file_acc:
     st.pyplot(fig)
     col1,col2 = st.columns(2)
     with col1:
-        fig, ax = plt.subplots(figsize=(10, 2))
+        fig, ax = plt.subplots(figsize=(10, 6))
         ax.plot(time_interpolated, v_acc, 'k-')
         ax.plot([0, 0], [0, 30], 'r--')
         ax.set_xlabel("Tempo (s)")
         ax.set_ylabel("Amplitude")
         st.pyplot(fig)
     with col2:    
-        fig, ax = plt.subplots(figsize=(10, 2))
+        fig, ax = plt.subplots(figsize=(10, 6))
         ax.plot(time_interpolated, ap_acc, 'k-')
         ax.plot([0, 0], [0, 30], 'r--')
         ax.set_xlabel("Tempo (s)")
