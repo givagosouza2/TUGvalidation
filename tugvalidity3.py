db7ef8376e901b40f318651aadf548727a10c2ad
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-10-27 10:31:13 -0300

    Update tugvalidity3.py

--- tugvalidity3.py
+++ tugvalidity3.py
@@ -168,8 +168,8 @@ with tab1:
 
         serie = pd.Series(v_gyro)
         # Parâmetros
-        window = 200
-        threshold = 0.5  # Limite para marcar no gráfico
+        window = 100
+        threshold = 0.3  # Limite para marcar no gráfico
 
         # Média móvel
         media_movel = pd.Series(v_gyro).rolling(window=window, min_periods=1).mean()
