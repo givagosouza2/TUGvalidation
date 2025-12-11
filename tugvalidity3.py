df0b41658705c6a9d3550dcbbca90101e4ef0e06
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-12-11 16:51:47 -0300

    Change cutoff frequency from 6Hz to 1.5Hz

--- tugvalidity3.py
+++ tugvalidity3.py
@@ -64,7 +64,7 @@ with tab1:
         # Pré-processamento
         do_detrend = True
         do_filter = True
-        cutoff_acc = 6  # Hz
+        cutoff_acc = 1.5  # Hz
 
         # Parâmetros de detecção
         height_thresh_ap = 3
