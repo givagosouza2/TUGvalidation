9e54a9a519c3bada68a4058392718bbe6f4e3c97
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-12-15 15:16:07 -0300

    Update tugvalidity2.py

--- tugvalidity2.py
+++ tugvalidity2.py
@@ -266,6 +266,7 @@ with tab1:
                 ax_v.set_xlabel("Tempo (s)")
                 ax_v.set_ylabel("Aceleração (Vertical)")
                 ax_v.legend(loc="lower left")
+                ax_v.grid(True,which="both",linestyle="--",linewidth=0.5,alpha=0.6)
                 st.pyplot(fig_v)
 
             # ---- Coluna 2: AP ----
@@ -307,6 +308,7 @@ with tab1:
                 ax_ap.set_xlabel("Tempo (s)")
                 ax_ap.set_ylabel("Aceleração (AP)")
                 ax_ap.legend(loc="lower left")
+                ax_ap.grid(True,which="both",linestyle="--",linewidth=0.5,alpha=0.6)
                 st.pyplot(fig_ap)
 
             # ===== Tabelas =====
